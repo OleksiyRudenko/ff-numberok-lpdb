b592d60706e08e80d9aeaf6c594958d3a0aca5c7
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2018-02-27 18:47:40 +0200

    Improve styling

--- root/index.php
+++ root/index.php
@@ -1,5 +1,5 @@
 <?php
-	include_once('../php-listener/time-tz-reset.php');
+	include_once('../ff-numberok-lpdb/time-tz-reset.php');
     $autorefresh = TRUE;
 	if (array_key_exists('autorefresh',$_GET))
 	   $autorefresh = $_GET['autorefresh'];
@@ -18,10 +18,67 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.4/css/all.css">
     <link rel="stylesheet" href="https://cdn.rawgit.com/OleksiyRudenko/normalize.css/28e3dc36/normalize.css">
     <link rel="stylesheet" href="https://cdn.rawgit.com/OleksiyRudenko/default-beauty.css/482728bd/default-beauty.css">
+    <style>
+        body {
+            margin-top: 0;
+            margin-bottom: 0;
+            padding-top: 0;
+            padding-bottom: 0;
+            height: 100%;
+            position: relative;
+        }
+        header {
+            position: absolute;
+            width: 100%;
+            flex-flow: column;
+        }
+        header > h1 {
+            margin-top: 0;
+        }
+        nav > * {
+            margin-right: 2rem;
+        }
+        nav a {
+            text-decoration-style: dotted;
+        }
+        footer {
+            position: absolute;
+            bottom: 0;
+            margin-bottom: 0;
+            width: 100%;
+        }
+        footer > * {
+            align-items: center;
+            margin-right: 2rem;
+        }
+        footer > p {
+            padding: 0;
+            margin: 0;
+        }
+        main {
+            height: 100vh;
+            margin-top: 0;
+            margin-bottom: 0;
+            padding-top: 5rem;
+            padding-bottom: 2.5rem;
+            overflow: auto;
+            box-sizing: border-box;
+        }
+    </style>
 </head>
 <body>
-	<h1>Simple HTTP listener</h1>
-	<a href='/index-dflt.php' target="_blank">Server info</a>
+    <header>
+        <h1>Simple NumberOK LP DB Manager</h1>
+        <nav>
+            <a href="?page=connect">Connect DB</a>
+            <a href="?page=groups">Manage groups</a>
+            <a href="?page=lp">Manage LPs</a>
+            <a href="?page=lpimport">Import LPs</a>
+        </nav>
+    </header>
+    <main>
+        <h2>Connecting to DB</h2>
+    </main>
 	<?php
     /*
 	include_once('../php-listener/db-connect.php');
@@ -60,5 +117,19 @@
 	</p>
     */
     ?>
+    <footer>
+        <a href="https://github.com/OleksiyRudenko/ff-numberok-lpdb" target="_blank">GitHub</a>
+        <a href="/index-dflt.php" target="_blank">Local Server info</a>
+        <p>
+            <small>Styled with
+                <a href="https://github.com/OleksiyRudenko/default-beauty.css" target="_blank">
+                    <i class="fas fa-heart"></i> Default-Beauty.css <i class="fab fa-github-square"></i>
+                </a>
+                and
+                <a href="https://github.com/OleksiyRudenko/normalize.css" target="_blank">
+                    normalize.css fork <i class="fab fa-github-square"></i>
+                </a></small>
+        </p>
+    </footer>
 </body>
 </html>
